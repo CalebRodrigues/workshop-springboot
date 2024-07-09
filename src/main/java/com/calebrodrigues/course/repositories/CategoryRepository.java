2d21deda47724632d5870f9a5f01eadc3fb262cd
Caleb <c4l3b23@gmail.com>
2024-07-09 16:40:55 -0300

    Category

---
+++ src/main/java/com/calebrodrigues/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.calebrodrigues.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.calebrodrigues.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
